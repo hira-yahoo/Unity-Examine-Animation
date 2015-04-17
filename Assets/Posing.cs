d5b5dc4078341380b7517d0150e32e34bf91f34c
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-17 18:07:30 +0900

    add a pose.

---
+++ Assets/Posing.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Posing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Animator anim = GetComponent<Animator> ();
+
+		if ( Input.GetKey ( KeyCode.A )) {
+			anim.SetBool( "Posing", true );
+		}
+
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Locomotion.wired_pose")) {
+			anim.SetBool( "Posing", false );
+		}
+
+	
+	}
+}
